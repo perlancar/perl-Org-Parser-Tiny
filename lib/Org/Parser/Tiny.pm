bce386d3bcf6ebbf2b77e51c6e138af123925711
perlancar (on pc-home) <perlancar@gmail.com>
2019-12-30 18:34:57 +0700

    Hackety hack

--- lib/Org/Parser/Tiny.pm
+++ lib/Org/Parser/Tiny.pm
@@ -1,9 +1,12 @@
-package Org::Parser::Tiny;
-
 use 5.010001;
 use strict;
 use warnings;
 
+package Org::Parser::Tiny;
+
+# AUTHORITY
+# DATE
+# DIST
 # VERSION
 
 sub new {
@@ -23,7 +26,8 @@ sub _parse {
         last if $lines->[0] =~ /^\*+ /;
         push @$curlines, shift(@$lines);
     }
-    push @res, {type=>'text', content=>join("",@$curlines)} if @$curlines;
+    push @res, Org::Parser::Tiny::Node::Head->new(
+        {type=>'text', content=>join("",@$curlines)} if @$curlines;
 
     # gather headlines + their content
 
@@ -46,7 +50,7 @@ sub parse {
     my ($self, $arg, $opts) = @_;
     die "Please specify a defined argument to parse()\n" unless defined($arg);
 
-    $opts //= {};
+    $opts ||= {};
 
     my $lines;
     my $r = ref($arg);
@@ -72,7 +76,7 @@ sub parse {
 
 sub parse_file {
     my ($self, $filename, $opts) = @_;
-    $opts //= {};
+    $opts ||= {};
 
     my $content = do {
         open my($fh), "<", $filename or die "Can't open $filename: $!\n";
@@ -82,6 +86,59 @@ sub parse_file {
 
     $self->parse($content, $opts);
 }
+
+
+# abstract class: Org::Parser::Tiny::Node
+package Org::Parser::Tiny::Node;
+
+sub new {
+    my ($class, %args) = @_;
+    bless \%args, $class;
+}
+
+sub parent { $_[0]{parent} }
+sub children { $_[0]{children} || [] }
+sub as_string { $_[0]{raw} }
+
+
+# abstract class: Org::Parser::Tiny::HasPreamble
+package Org::Parser::Tiny::Node::HasPreamble;
+
+our @ISA = qw(Org::Parser::Tiny::Node);
+
+sub new {
+    my ($class, %args) = @_;
+    $args{preamble} //= "";
+    $class->SUPER::new(%args);
+}
+
+sub as_string {
+    $_[0]->{preamble} . join("", map { $_->as_string } @{ $_[0]->children });
+}
+
+
+# class: Org::Parser::Tiny::Document: top level node
+package Org::Parser::Tiny::Node::Document;
+
+our @ISA = qw(Org::Parser::Tiny::Node::HasPreamble);
+
+
+# class: Org::Parser::Tiny::Node::Headline: headline with its content
+package Org::Parser::Tiny::Node::Headline;
+
+our @ISA = qw(Org::Parser::Tiny::Node::HasPreamble);
+
+sub level { $_[0]{level} }
+
+sub tree { $_[0]{tree} }
+
+sub is_todo { $_[0]{is_todo} }
+
+sub is_done { $_[0]{is_done} }
+
+sub todo_state { $_[0]{todo_state} }
+
+sub tags { $_[0]{tags} || [] }
 
 1;
 # ABSTRACT: Parse Org documents with as little code (and no non-core deps) as possible
@@ -101,11 +158,43 @@ sub parse_file {
  ** this is yet another headline
  EOF
 
+Dump document structure using L<Tree::Dump>:
+
+ use Tree::Dump;
+ td($doc);
+
+Select document nodes using L<Data::CSel>:
+
+ use Data::CSel qw(csel);
+
+ # select headlines with "foo" in their title
+ my @nodes = csel(
+     {class_prefixes => ["Org::Parser::Tiny::Node"]},
+     "Headline[title =~ /foo/]"
+ );
+
+Manipulate tree nodes with path-like semantic using L<Tree::FSLike>:
+
+ use Tree::FSLike;
+ my $fs = Tree::FSLike->new(
+     tree => $doc,
+     gen_filename_method => sub { $_[0]->can("title") ? $_[0]->title : "$_[0]" },
+ );
+
+ # list nodes right above the root node
+ my @nodes = $fs->ls;
+
+ # use wildcard to list nodes
+ my @nodes = $fs->ls("*foo*");
+
+ # remove top-level headlines which have "foo" in their title
+ $fs->rm($doc, "/*foo*");
+
 
 =head1 DESCRIPTION
 
 This module is a more lightweight alternative to L<Org:Parser>. Currently it is
-very simple and can only parse headlines. I use this to write utilities like
+very simple and only parses headlines. I use this to write utilities like
 L<sort-org-headlines-tiny>.
 
 
@@ -114,17 +203,29 @@ L<sort-org-headlines-tiny>.
 
 =head1 METHODS
 
-=head2 new()
+=head2 new
 
-Create a new parser instance.
+Usage:
 
-=head2 $orgp->parse($str | $arrayref | $coderef | $filehandle, \%opts) => $doc
+ my $orgp = Org::Parser::Tiny->new;
 
-Parse document (which can be contained in a scalar $str, an array of lines
-$arrayref, a subroutine which will be called for chunks until it returns undef,
-or a filehandle).
+Constructor. Create a new parser instance.
 
-Returns Perl structure representing the document.
+=head2 parse
+
+Usage:
+
+ my $doc = $orgp->parse($str | $arrayref | $coderef | $filehandle, \%opts);
+
+Parse document (which can be contained in a $str, an array of lines $arrayref, a
+$coderef which will be called for chunks until it returns undef, or a
+$filehandle.
+
+Returns a tree of node objects (of class C<Org::Parser::Tiny::Node> and its
+subclasses C<Org::Parser::Tiny::Node::Document> and
+C<Org::Parser::Tiny::Node::Headline>). The tree node complies to
+L<Role::TinyCommons::Tree::Node> role, so these tools are available:
+L<Data::CSel>, L<Tree::Dump>, L<Tree::FSLike>, etc.
 
 Will die if there are syntax errors in documents.
 
@@ -134,9 +235,13 @@ Known options:
 
 =back
 
-=head2 $orgp->parse_file($filename, \%opts) => $doc
+=head2 parse_file
 
-Just like parse(), but will load document from file instead.
+Usage:
+
+ my $doc = $orgp->parse_file($filename, \%opts);
+
+Just like L</parse>, but will load document from file instead.
 
 Known options (aside from those known by parse()):
 
@@ -150,6 +255,8 @@ Known options (aside from those known by parse()):
 
 =head1 SEE ALSO
 
-L<Org::Parser>
+L<Org::Parser>, the more fully featured Org parser.
+
+L<https://orgmode.org>
 
 =cut
